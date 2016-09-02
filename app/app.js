b1add78e4d37b95f3af2752c387c6e71ce8db880
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-02 11:53:51 +0200

    update react-router-scroll

--- app/app.js
+++ app/app.js
@@ -18,7 +18,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
-import useScroll from 'react-router-scroll'
+import { useScroll } from 'react-router-scroll'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
