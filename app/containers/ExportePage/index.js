424af2c5b7e56a1d73a30f927bd8aee735247c8b
Alexander Gabriel <alex.barbalex@gmail.com>
2016-09-02 17:58:35 +0200

    add helmet

--- app/containers/ExportePage/index.js
+++ app/containers/ExportePage/index.js
@@ -10,12 +10,19 @@
  */
 
 import React from 'react'
+import Helmet from 'react-helmet'
 
 export default class ExportePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
       <div>
+        <Helmet
+          title="AP Flora Exporte"
+          meta={[
+            { name: 'description', content: 'Exporte von apfora.ch' },
+          ]}
+        />
         this is Exporte page
       </div>
     )
