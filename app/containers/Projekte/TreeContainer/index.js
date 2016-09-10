5e7d249b6f323fce85d9dbb54992c4cf3aa07f03
Alexander Gabriel <alex@gabriel-software.ch>
2016-09-10 12:06:12 +0200

    bind tabs to store

--- app/containers/Projekte/TreeContainer/index.js
+++ app/containers/Projekte/TreeContainer/index.js
@@ -4,29 +4,50 @@
  *
  */
 
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import { observer } from 'mobx-react'
 import { Tabs, Tab } from 'material-ui/Tabs'
 import styles from './styles.css'
 import Filter from './Filter'
 import Tree from './Tree'
 
-export default class TreeContainer extends Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div className={styles.tree}>
-        <Tabs>
-          <Tab
-            label="Strukturbaum"
-            value="strukturbaum"
-          />
-          <Tab
-            label="Filter"
-            value="filter"
-          />
-        </Tabs>
-        <Tree />
-        <Filter />
-      </div>
-    )
+const TreeContainer = observer(
+  class TreeContainer extends Component { // eslint-disable-line react/prefer-stateless-function
+    static contextTypes = {
+      router: React.PropTypes.object.isRequired,
+      store: React.PropTypes.object.isRequired,
+    }
+
+    onClickTab = (value) => {
+      const { store } = this.context
+      store.ui.projekteViews.strukturbaum.activeTab = value
+    }
+
+    render() {
+      const { store } = this.context
+      return (
+        <div className={styles.strukturbaum}>
+          <Tabs
+            value={store.ui.projekteViews.strukturbaum.activeTab}
+            onChange={this.onClickTab}
+          >
+            <Tab
+              label="Strukturbaum"
+              value="strukturbaum"
+            >
+              <Tree />
+            </Tab>
+            <Tab
+              label="Filter"
+              value="filter"
+            >
+              <Filter />
+            </Tab>
+          </Tabs>
+        </div>
+      )
+    }
   }
-}
+)
+
+export default TreeContainer
