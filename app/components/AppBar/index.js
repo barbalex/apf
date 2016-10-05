e2e744383e01d40661e656ebb8b0dd03f2fba590
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-05 11:49:45 +0200

    repair navigation

--- app/components/AppBar/index.js
+++ app/components/AppBar/index.js
@@ -5,64 +5,62 @@ import IconMenu from 'material-ui/IconMenu'
 import MenuItem from 'material-ui/MenuItem'
 import IconButton from 'material-ui/IconButton/IconButton'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
-import { browserHistory } from 'react-router'
 
 import styles from './styles.css'
 
-const MyAppBar = (props) => {
-  const { location } = props
-  const onClickTab = (path) => {
-    browserHistory.push(path)
-  }
-
-  return (
-    <AppBar
-      title="AP Flora"
-      className={styles.menuDiv}
-      iconElementRight={
-        <div
-          className={styles.menuDiv}
+const MyAppBar = ({ pathname }, context) =>
+  <AppBar
+    title="AP Flora"
+    className={styles.menuDiv}
+    iconElementRight={
+      <div
+        className={styles.menuDiv}
+      >
+        <Tabs
+          value={pathname}
+          onChange={(path) => {
+            context.router.transitionTo(path)
+          }}
         >
-          <Tabs
-            value={location.pathname}
-            onChange={onClickTab}
-          >
-            <Tab
-              label="Projekte"
-              value="/Projekte"
-              className={styles.tab}
-            />
-            <Tab
-              label="Exporte"
-              value="/Exporte"
-              className={styles.tab}
-            />
-            <Tab
-              label="Benutzer"
-              value="/Benutzer"
-              className={styles.tab}
-            />
-          </Tabs>
-          <IconMenu
-            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-            anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
-            style={{
-              paddingLeft: 10,
-            }}
-          >
-            <MenuItem
-              primaryText="Über apflora.ch"
-              onTouchTap={() =>
-                window.open('https://github.com/FNSKtZH/apflora/wiki')
-              }
-            />
-          </IconMenu>
-        </div>
-      }
-      showMenuIconButton={false}
-    />
-  )
+          <Tab
+            label="Projekte"
+            value="/Projekte"
+            className={styles.tab}
+          />
+          <Tab
+            label="Exporte"
+            value="/Exporte"
+            className={styles.tab}
+          />
+          <Tab
+            label="Benutzer"
+            value="/Benutzer"
+            className={styles.tab}
+          />
+        </Tabs>
+        <IconMenu
+          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          style={{ paddingLeft: 10 }}
+        >
+          <MenuItem
+            primaryText="Über apflora.ch"
+            onTouchTap={() =>
+              window.open('https://github.com/FNSKtZH/apflora/wiki')
+            }
+          />
+        </IconMenu>
+      </div>
+    }
+    showMenuIconButton={false}
+  />
+
+MyAppBar.contextTypes = {
+  router: React.PropTypes.object,
+}
+MyAppBar.propTypes = {
+  pathname: React.PropTypes.string,
 }
 
 export default MyAppBar
