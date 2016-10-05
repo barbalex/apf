31d6dc93533e6dc7aab8c46301f54f3c93364df3
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-05 09:50:05 +0200

    correct error

--- app/modules/tables.js
+++ app/modules/tables.js
@@ -178,7 +178,7 @@ export default [
     form: 'tpopmassnber',
     initiiereFunktion: 'initiiereTPopMassnBer',
     treeTyp: 'tpopmassnber',
-  },,
+  },
   {
     database: 'apflora',
     tabelleInDb: 'beobzuordnung',
