4e0fa6a2d546dc1da00fe760077fdbd4cd8bb373
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-05 09:49:27 +0200

    get first array object

--- app/modules/fetchDataset.js
+++ app/modules/fetchDataset.js
@@ -3,7 +3,7 @@ import apiBaseUrl from './apiBaseUrl'
 export default ({ table, field, value }) =>
   fetch(`${apiBaseUrl}/apflora/tabelle=${table}/feld=${field}/${Number.isNaN(value) ? 'wertString' : 'wertNumber'}=${value}`)
     .then(resp => resp.json())
-    .then(dataset => dataset)
+    .then(dataset => dataset[0])
     .catch((error) => {
       throw error
     })
