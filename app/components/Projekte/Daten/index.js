8bb0d396b3c904b2950127ef1d46e3e308d95a28
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-06 12:32:24 +0100

    prepare showing pop

--- app/components/Projekte/Daten/index.js
+++ app/components/Projekte/Daten/index.js
@@ -7,21 +7,36 @@
 import React, { Component, PropTypes } from 'react'
 import { observer, inject } from 'mobx-react'
 import styles from './styles.css'
+import Pop from './Pop'
 
 const Daten = class Daten extends Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
+  constructor() {
+    super()
+    this.activeForm = this.activeForm.bind(this)
+  }
+  activeForm() {
     const { store } = this.props
-    return (
-      <div className={styles.container}>
-        <p>Daten</p>
-        <pre>
-          {
-            store.data.activeDataset.row
-            && JSON.stringify(store.data.activeDataset, null, 2)
-          }
-        </pre>
-      </div>
-    )
+    if (!store.data.activeDataset || !store.data.activeDataset.table) {
+      return <div></div>
+    }
+    switch (store.data.activeDataset.table) {
+      case 'pop':
+        return <Pop />
+      default:
+        return (
+          store.data.activeDataset.row &&
+          <div className={styles.container}>
+            <p>Daten</p>
+            <pre>
+              {JSON.stringify(store.data.activeDataset, null, 2)}
+            </pre>
+          </div>
+        )
+    }
+  }
+
+  render() {
+    return this.activeForm()
   }
 }
 
