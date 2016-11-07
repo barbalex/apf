ac18256953a5a78a5957cc563d660739efff9274
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-07 17:42:25 +0100

    initiate map

--- app/components/Projekte/Karte/index.js
+++ app/components/Projekte/Karte/index.js
@@ -6,13 +6,20 @@
 
 import React from 'react'
 import { observer, inject } from 'mobx-react'
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import styles from './styles.css'
 
 const Karte = class Karte extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const position = [47.295, 8.58]
     return (
       <div className={styles.container}>
-        Karte
+        <Map center={position} zoom={13} className={styles.map}>
+          <TileLayer
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </Map>
       </div>
     )
   }
