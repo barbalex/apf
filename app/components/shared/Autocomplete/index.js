95de7e12ccf107e1364368deb23cc971a42a87af
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-07 22:17:06 +0100

    make function

--- app/components/shared/Autocomplete/index.js
+++ app/components/shared/Autocomplete/index.js
@@ -1,49 +1,39 @@
 import React, { Component, PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import AutoComplete from 'material-ui/AutoComplete'
-import styles from './styles.css'
 
-const Autocomplete = class Autocomplete extends Component { // eslint-disable-line react/prefer-stateless-function
-
-  constructor() {
-    super()
-
+const Autocomplete = () => {
+  const {
+    label,
+    fieldName,
+    value = ``,
+    dataSource,
+    dataSourceConfig = {
+      value: `id`,
+      text: `label`,
+    },
+    onChange,
+  } = this.props
+  let searchText = ``
+  if (value && dataSource.length > 0) {
+    searchText = dataSource.find(e => e.id === value).label
   }
-
-  render() {
-    const {
-      label,
-      fieldName,
-      value = ``,
-      dataSource,
-      dataSourceConfig = {
-        value: `id`,
-        text: `label`,
-      },
-      onChange,
-    } = this.props
-    let searchText = ``
-    if (value && dataSource.length > 0) {
-      searchText = dataSource.find(e => e.id === value).label
-    }
-    return (
-      <AutoComplete
-        hintText={dataSource.length === 0 ? `lade Daten...` : ``}
-        fullWidth
-        floatingLabelText={label}
-        openOnFocus
-        dataSource={dataSource}
-        dataSourceConfig={dataSourceConfig}
-        searchText={searchText}
-        filter={AutoComplete.caseInsensitiveFilter}
-        maxSearchResults={20}
-        onNewRequest={(value) => {
-          console.log(`value clicked:`, value.id)
-          onChange(fieldName, value.id)
-        }}
-      />
-    )
-  }
+  return (
+    <AutoComplete
+      hintText={dataSource.length === 0 ? `lade Daten...` : ``}
+      fullWidth
+      floatingLabelText={label}
+      openOnFocus
+      dataSource={dataSource}
+      dataSourceConfig={dataSourceConfig}
+      searchText={searchText}
+      filter={AutoComplete.caseInsensitiveFilter}
+      maxSearchResults={20}
+      onNewRequest={val =>
+        onChange(fieldName, val.id)
+      }
+    />
+  )
 }
 
 Autocomplete.propTypes = {
