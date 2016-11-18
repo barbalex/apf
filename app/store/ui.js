31f0abcd37d03e4fae48298b5e6f45ff6c3c43be
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-18 10:31:32 +0100

    move state

--- app/store/ui.js
+++ app/store/ui.js
@@ -4,6 +4,7 @@ import $ from 'jquery'
 class Ui {
   @observable windowWidth = $(window).width()
   @observable windowHeight = $(window).height()
+  @observable treeHeight = 0
   @observable lastClickY = 0
   @observable treeTopPosition = 0
   @observable projekte = {
