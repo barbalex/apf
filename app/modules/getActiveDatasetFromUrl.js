3693469e0937289ebde3604af81898528221d181
Alexander Gabriel <alex.barbalex@gmail.com>
2016-11-29 13:52:54 +0100

    set correct null values

--- app/modules/getActiveDatasetFromUrl.js
+++ app/modules/getActiveDatasetFromUrl.js
@@ -2,57 +2,62 @@ import getActiveUrlElements from './getActiveUrlElements'
 
 export default (store) => {
   const aEl = getActiveUrlElements()
+  let activeDataset = {
+    table: null,
+    row: null,
+    valid: {},
+  }
   if (aEl.projekt) {
     if (aEl.apberuebersicht) {
-      return { table: `apberuebersicht`, row: store.table.apberuebersicht.get(aEl.apberuebersicht) }
+      activeDataset = { table: `apberuebersicht`, row: store.table.apberuebersicht.get(aEl.apberuebersicht) }
     } else if (aEl.ap) {
       if (aEl.ziel) {
         if (aEl.zielber) {
-          return { table: `zielber`, row: store.table.zielber.get(aEl.zielber) }
+          activeDataset = { table: `zielber`, row: store.table.zielber.get(aEl.zielber) }
         }
-        return { table: `ziel`, row: store.table.ziel.get(aEl.ziel) }
+        activeDataset = { table: `ziel`, row: store.table.ziel.get(aEl.ziel) }
       } else if (aEl.erfkrit) {
-        return { table: `erfkrit`, row: store.table.erfkrit.get(aEl.erfkrit) }
+        activeDataset = { table: `erfkrit`, row: store.table.erfkrit.get(aEl.erfkrit) }
       } else if (aEl.apber) {
-        return { table: `apber`, row: store.table.apber.get(aEl.apber) }
+        activeDataset = { table: `apber`, row: store.table.apber.get(aEl.apber) }
       } else if (aEl.ber) {
-        return { table: `ber`, row: store.table.ber.get(aEl.ber) }
+        activeDataset = { table: `ber`, row: store.table.ber.get(aEl.ber) }
       } else if (aEl.beobzuordnung) {
-        return { table: `beobzuordnung`, row: store.table.beobzuordnung.get(aEl.beobzuordnung) }
+        activeDataset = { table: `beobzuordnung`, row: store.table.beobzuordnung.get(aEl.beobzuordnung) }
       } else if (aEl.beobNichtZuzuordnen) {
-        return { table: `beobNichtZuzuordnen`, row: store.table.beobNichtZuzuordnen.get(aEl.beobNichtZuzuordnen) }
+        activeDataset = { table: `beobNichtZuzuordnen`, row: store.table.beobNichtZuzuordnen.get(aEl.beobNichtZuzuordnen) }
       } else if (aEl.assozart) {
-        return { table: `assozart`, row: store.table.assozart.get(aEl.assozart) }
+        activeDataset = { table: `assozart`, row: store.table.assozart.get(aEl.assozart) }
       } else if (aEl.pop) {
         if (aEl.tpopmassnber) {
-          return { table: `tpopmassnber`, row: store.table.tpopmassnber.get(aEl.tpopmassnber) }
+          activeDataset = { table: `tpopmassnber`, row: store.table.tpopmassnber.get(aEl.tpopmassnber) }
         } else if (aEl.tpopber) {
-          return { table: `tpopber`, row: store.table.tpopber.get(aEl.tpopber) }
+          activeDataset = { table: `tpopber`, row: store.table.tpopber.get(aEl.tpopber) }
         } else if (aEl.tpop) {
           if (aEl.tpopBeobzuordnung) {
-            return { table: `tpopBeobzuordnung`, row: store.table.tpopBeobzuordnung.get(aEl.tpopBeobzuordnung) }
+            activeDataset = { table: `tpopBeobzuordnung`, row: store.table.tpopBeobzuordnung.get(aEl.tpopBeobzuordnung) }
           } else if (aEl.tpopber) {
-            return { table: `tpopber`, row: store.table.tpopber.get(aEl.tpopber) }
+            activeDataset = { table: `tpopber`, row: store.table.tpopber.get(aEl.tpopber) }
           } else if (aEl.tpopfreiwkontr) {
-            return { table: `tpopfreiwkontr`, row: store.table.tpopfreiwkontr.get(aEl.tpopfreiwkontr) }
+            activeDataset = { table: `tpopfreiwkontr`, row: store.table.tpopfreiwkontr.get(aEl.tpopfreiwkontr) }
           } else if (aEl.tpopfeldkontr) {
-            return { table: `tpopfeldkontr`, row: store.table.tpopfeldkontr.get(aEl.tpopfeldkontr) }
+            activeDataset = { table: `tpopfeldkontr`, row: store.table.tpopfeldkontr.get(aEl.tpopfeldkontr) }
           } else if (aEl.tpopmassnber) {
-            return { table: `tpopmassnber`, row: store.table.tpopmassnber.get(aEl.tpopmassnber) }
+            activeDataset = { table: `tpopmassnber`, row: store.table.tpopmassnber.get(aEl.tpopmassnber) }
           } else if (aEl.tpopmassn) {
-            return { table: `tpopmassn`, row: store.table.tpopmassn.get(aEl.tpopmassn) }
+            activeDataset = { table: `tpopmassn`, row: store.table.tpopmassn.get(aEl.tpopmassn) }
           }
-          return { table: `tpop`, row: store.table.tpop.get(aEl.tpop) }
+          activeDataset = { table: `tpop`, row: store.table.tpop.get(aEl.tpop) }
         }
         // none of the tpop folders is active
-        return { table: `pop`, row: store.table.pop.get(aEl.pop) }
+        activeDataset = { table: `pop`, row: store.table.pop.get(aEl.pop) }
       }
       // none of the pop folders is active
-      return { table: `ap`, row: store.table.ap.get(aEl.ap) }
+      activeDataset = { table: `ap`, row: store.table.ap.get(aEl.ap) }
     }
     // !aEl.ap && !aEl.apberuebersicht
-    return { table: `projekt`, row: store.table.projekt.get(aEl.projekt) }
+    activeDataset = { table: `projekt`, row: store.table.projekt.get(aEl.projekt) }
   }
-  // no aEl.projekt
-  return null
+  console.log(`getActiveDatasetFromUrl: activeDataset:`, activeDataset)
+  return activeDataset
 }
