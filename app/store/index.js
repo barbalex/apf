e5f8655e01331c26e2baa63de92ccc46e30e3871
Alexander Gabriel <alex@gabriel-software.ch>
2016-12-02 13:50:41 +0100

    modularize assozart nodes

--- app/store/index.js
+++ app/store/index.js
@@ -29,6 +29,7 @@ import fetchDataForActiveUrlElements from '../modules/fetchDataForActiveUrlEleme
 import buildBerNodes from '../modules/nodes/ber'
 import buildErfkritNodes from '../modules/nodes/erfkrit'
 import buildApberNodes from '../modules/nodes/apber'
+import buildAssozartNodes from '../modules/nodes/assozart'
 
 import NodeStore from './node'
 import UiStore from './ui'
@@ -273,42 +274,7 @@ class Store extends singleton {
   }
 
   @computed get assozartNodes() {
-    const { activeUrlElements } = this
-    // grab assozart as array and sort them by year
-    let assozart = Array.from(this.table.assozart.values())
-    // show only nodes of active ap
-    const activeAp = this.activeUrlElements.ap
-    assozart = assozart.filter(a => a.AaApArtId === activeAp)
-    // map through all projekt and create array of nodes
-    let nodes = assozart.map((el) => {
-      let label = `...`
-      const { adb_eigenschaften } = this.table
-      if (adb_eigenschaften.size > 0) {
-        if (el.AaSisfNr) {
-          label = adb_eigenschaften.get(el.AaSisfNr).Artname
-        } else {
-          label = `(keine Art gewählt)`
-        }
-      }
-      const projId = this.table.ap.get(el.AaApArtId).ProjId
-      return {
-        type: `row`,
-        label,
-        table: `assozart`,
-        row: el,
-        expanded: el.AaId === activeUrlElements.assozart,
-        url: [`Projekte`, projId, `Arten`, el.AaApArtId, `assoziierte-Arten`, el.AaId],
-      }
-    })
-    // filter by node.nodeLabelFilter
-    const filterString = this.node.nodeLabelFilter.get(`assozart`)
-    if (filterString) {
-      nodes = nodes.filter(p =>
-        p.label.toLowerCase().includes(filterString.toLowerCase())
-      )
-    }
-    // sort by label and return
-    return sortBy(nodes, `label`)
+    return buildAssozartNodes(this)
   }
 
   @computed get apberNodes() {
