e5c5765c4873d2bfcb6ec7bf376f59ac6da833d8
Alexander Gabriel <alex@gabriel-software.ch>
2016-12-23 09:16:02 +0100

    work on exdporte

--- app/components/Projekte/Daten/Exporte/index.js
+++ app/components/Projekte/Daten/Exporte/index.js
@@ -22,6 +22,7 @@ const FieldsContainer = styled.div`
 const FirstLevelCard = styled(Card)`
   margin-bottom: 10px;
 `
+const DownloadCardText = styled
 
 export default class Exporte extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor() {
@@ -107,6 +108,60 @@ export default class Exporte extends React.Component { // eslint-disable-line re
                   })
                 }
               />
+              <FlatButton
+                label="Ziele"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_ziel`,
+                    fileName: `ApZiele`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Ziel-Berichte"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_zielber`,
+                    fileName: `Zielberichte`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Berichte"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_ber`,
+                    fileName: `Berichte`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Erfolgskriterien"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_erfkrit`,
+                    fileName: `Erfolgskriterien`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Idealbiotope"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_idealbiotop`,
+                    fileName: `Idealbiotope`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Assoziierte Arten"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_assozart`,
+                    fileName: `AssoziierteArten`,
+                  })
+                }
+              />
             </CardText>
           </FirstLevelCard>
           <FirstLevelCard>
@@ -118,6 +173,114 @@ export default class Exporte extends React.Component { // eslint-disable-line re
             <CardText
               expandable
             >
+              <FlatButton
+                label="Populationen"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop`,
+                    fileName: `Populationen`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen für Google Earth (beschriftet mit PopNr)"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_kml`,
+                    fileName: `Populationen`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen für Google Earth (beschriftet mit Artname, PopNr)"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_kmlnamen`,
+                    fileName: `PopulationenNachNamen`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen ohne Teilpopulationen"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_ohnetpop`,
+                    fileName: `PopulationenOhneTeilpopulationen`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen von AP-Arten ohne Status"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_vonapohnestatus`,
+                    fileName: `PopulationenVonApArtenOhneStatus`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen ohne Koordinaten"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_ohnekoord`,
+                    fileName: `PopulationenOhneKoordinaten`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen mit Massnahmen-Berichten: Anzahl Massnahmen im Berichtsjahr"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_popmassnber_anzmassn`,
+                    fileName: `PopulationenAnzMassnProMassnber`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Anzahl Massnahmen pro Population"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_anzmassn`,
+                    fileName: `PopulationenAnzahlMassnahmen`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Anzahl Kontrollen pro Population"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_anzkontr`,
+                    fileName: `PopulationenAnzahlKontrollen`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen inkl. Populations- und Massnahmen-Berichte"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_popberundmassnber`,
+                    fileName: `PopulationenPopUndMassnBerichte`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen mit dem letzten Populations-Bericht"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_mit_letzter_popber`,
+                    fileName: `PopulationenMitLetzemPopBericht`,
+                  })
+                }
+              />
+              <FlatButton
+                label="Populationen mit dem letzten Massnahmen-Bericht"
+                onClick={() =>
+                  this.downloadFromView({
+                    view: `v_pop_mit_letzter_popmassnber`,
+                    fileName: `PopulationenMitLetztemMassnBericht`,
+                  })
+                }
+              />
             </CardText>
           </FirstLevelCard>
           <FirstLevelCard>
