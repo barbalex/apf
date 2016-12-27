bab4359a7b0c9fbaf2bda7fca6ea3b6105e27014
Alexander Gabriel <alex.barbalex@gmail.com>
2016-12-27 22:12:30 +0100

    show correct nodes for beob nicht beurteilt

--- app/modules/nodes/beobzuordnung.js
+++ app/modules/nodes/beobzuordnung.js
@@ -5,7 +5,15 @@ export default (store, apArtId) => {
   // grab beob_bereitgestellt as array and sort them by year
   let beobBereitgestellt = Array.from(table.beob_bereitgestellt.values())
   // show only nodes of active ap
-  beobBereitgestellt = beobBereitgestellt.filter(a => a.NO_ISFS === apArtId)
+  beobBereitgestellt = beobBereitgestellt.filter(a =>
+    a.NO_ISFS === apArtId &&
+    (
+      (a.beobzuordnung &&
+      a.beobzuordnung.type &&
+      a.beobzuordnung.type === `nichtBeurteilt`) ||
+      !a.beobzuordnung
+    )
+  )
   // map through all and create array of nodes
   let nodes = beobBereitgestellt.map((el) => {
     const quelle = table.beob_quelle.get(el.QuelleId)
