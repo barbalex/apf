18021ddac11d4bac9c84c119c6f5b55c3b466b71
Alexander Gabriel <alex.barbalex@gmail.com>
2016-12-30 22:04:28 +0100

    try to add bing map

---
+++ app/components/Projekte/Karte/layers/BingAerial.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import BingLayer from 'react-leaflet-bing'
+
+const BingAerialLayer = () =>
+  <BingLayer
+    bingkey="AjGOtB_ygBplpxXtKiiHtm-GERjSg9TFEoCmuBI_Yz4VWy0unRGUDo9GOZHA46Pf"
+  />
+
+export default BingAerialLayer
