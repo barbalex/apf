3d778fa7e67d872541c4907bbf1a9fe24f62d8c4
Alexander Gabriel <alex.barbalex@gmail.com>
2017-01-01 12:26:15 +0100

    try to get bing map working

---
+++ app/components/Projekte/Karte/indexWithBing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Map } from 'react-leaflet'
+
+import BingAerial from './layers/BingAerial'
+
+class Karte extends React.Component {
+  render() {
+    const position = [47.295, 8.58]
+
+    return (
+      <Map
+        center={position}
+        zoom={13}
+      >
+        <BingAerial />
+      </Map>
+    )
+  }
+}
+
+export default Karte
--- app/components/Projekte/Karte/layers/BingAerial.js
+++ app/components/Projekte/Karte/layers/BingAerial.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import BingLayer from 'react-leaflet-bing'
+    import React from 'react'
+    import BingLayer from 'react-leaflet-bing'
 
-const BingAerialLayer = () =>
-  <BingLayer
-    bingkey="AjGOtB_ygBplpxXtKiiHtm-GERjSg9TFEoCmuBI_Yz4VWy0unRGUDo9GOZHA46Pf"
-  />
+    const bingKey = `AjGOtB_ygBplpxXtKiiHtm-GERjSg9TFEoCmuBI_Yz4VWy0unRGUDo9GOZHA46Pf`
 
-export default BingAerialLayer
+    const BingAerialLayer = () =>
+      <BingLayer
+        bingkey={bingKey}
+        type="Road"
+      />
+
+    export default BingAerialLayer
