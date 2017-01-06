8b6fc8955d1b3f09dae62224f66664a5c11aab9f
Alexander Gabriel <alex.barbalex@gmail.com>
2017-01-06 17:18:13 +0100

    optimize

--- app/modules/nodes/projekt.js
+++ app/modules/nodes/projekt.js
@@ -19,26 +19,23 @@ export default (store) => {
   // sort
   projekte = sortBy(projekte, `ProjName`)
   // map through all projekt and create array of nodes
-  return projekte.map((el) => {
-
-    return {
-      nodeType: `table`,
-      menuType: `projekt`,
-      id: el.ProjId,
-      label: el.ProjName || `(kein Name)`,
-      expanded: el.ProjId === activeUrlElements.projekt,
-      url: [`Projekte`, el.ProjId],
-      children: [
-        apFolderNode(store, el.ProjId),
-        apberuebersichtFolderNode(store, el.ProjId),
-        {
-          menuType: `exporte`,
-          id: el.ProjId,
-          label: `Exporte`,
-          expanded: activeUrlElements.exporte,
-          url: [`Projekte`, el.ProjId, `Exporte`],
-        },
-      ],
-    }
-  })
+  return projekte.map(el => ({
+    nodeType: `table`,
+    menuType: `projekt`,
+    id: el.ProjId,
+    label: el.ProjName || `(kein Name)`,
+    expanded: el.ProjId === activeUrlElements.projekt,
+    url: [`Projekte`, el.ProjId],
+    children: [
+      apFolderNode(store, el.ProjId),
+      apberuebersichtFolderNode(store, el.ProjId),
+      {
+        menuType: `exporte`,
+        id: el.ProjId,
+        label: `Exporte`,
+        expanded: activeUrlElements.exporte,
+        url: [`Projekte`, el.ProjId, `Exporte`],
+      },
+    ],
+  }))
 }
