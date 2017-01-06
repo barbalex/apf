09f6d5f0717b64aa962511828a7a548bfc3145c6
Alexander Gabriel <alex.barbalex@gmail.com>
2017-01-06 17:35:02 +0100

    enhance filter message

--- app/modules/nodes/popFolder.js
+++ app/modules/nodes/popFolder.js
@@ -3,12 +3,19 @@ import popNodes from './pop'
 export default (store, projId, apArtId) => {
   const { activeUrlElements } = store
   const myPopNodes = popNodes(store, apArtId)
+  let message = myPopNodes.length
+  if (store.table.popLoading) {
+    message = `...`
+  }
+  if (store.node.nodeLabelFilter.get(`pop`)) {
+    message = `${myPopNodes.length} gefiltert`
+  }
 
   return {
     nodeType: `folder`,
     menuType: `popFolder`,
     id: apArtId,
-    label: `Populationen (${myPopNodes.length})`,
+    label: `Populationen (${message})`,
     expanded: activeUrlElements.popFolder,
     url: [`Projekte`, projId, `Arten`, apArtId, `Populationen`],
     children: myPopNodes,
