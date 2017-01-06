7ffe27c551a0e63361671ab84127a50c6002f8e2
Alexander Gabriel <alex.barbalex@gmail.com>
2017-01-06 12:55:32 +0100

    cache tpopForAp in idb

--- app/modules/fetchTpopForAp.js
+++ app/modules/fetchTpopForAp.js
@@ -1,6 +1,16 @@
 import { transaction } from 'mobx'
 import axios from 'axios'
+import app from 'ampersand-app'
+
 import apiBaseUrl from './apiBaseUrl'
+
+const writeToStore = (store, data) => {
+  transaction(() => {
+    data.forEach(d =>
+      store.table.tpop.set(d.TPopId, d)
+    )
+  })
+}
 
 export default (store, apArtId) => {
   if (!apArtId) {
@@ -8,13 +18,18 @@ export default (store, apArtId) => {
   }
 
   const url = `${apiBaseUrl}/tpopForAp/${apArtId}`
-  return axios.get(url)
+
+  app.db.tpop
+    .toArray()
+    .then((data) => {
+      if (data) {
+        writeToStore(store, data)
+      }
+    })
+    .then(() => axios.get(url))
     .then(({ data }) => {
-      transaction(() => {
-        data.forEach(d =>
-          store.table.tpop.set(d.TPopId, d)
-        )
-      })
+      writeToStore(store, data)
+      app.db.tpop.bulkPut(data)
     })
     .catch(error => new Error(`error fetching tpop for ap ${apArtId}:`, error))
 }
