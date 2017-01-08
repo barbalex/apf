2fc6e7cdb564266a8f763e5d109c5adeccc582b3
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-08 15:09:35 +0100

    enhance loading only once

--- app/modules/fetchTpopForAp.js
+++ app/modules/fetchTpopForAp.js
@@ -4,17 +4,38 @@ import app from 'ampersand-app'
 
 import apiBaseUrl from './apiBaseUrl'
 
-const writeToStore = (store, data) => {
+const writeToStore = (store, data, apArtId) => {
+  const { valuesForWhichTableDataWasFetched } = store
   transaction(() => {
     data.forEach(d =>
       store.table.tpop.set(d.TPopId, d)
     )
   })
+  // record that data was fetched for this value
+  if (!valuesForWhichTableDataWasFetched.tpopForAp) {
+    valuesForWhichTableDataWasFetched.tpopForAp = {}
+  }
+  if (!valuesForWhichTableDataWasFetched.tpopForAp.ApArtId) {
+    valuesForWhichTableDataWasFetched.tpopForAp.ApArtId = []
+  }
+  if (!valuesForWhichTableDataWasFetched.tpopForAp.ApArtId.includes(apArtId)) {
+    valuesForWhichTableDataWasFetched.tpopForAp.ApArtId.push(apArtId)
+  }
 }
 
 export default (store, apArtId) => {
   if (!apArtId) {
     return new Error(`action fetchTpopForAp: apArtId must be passed`)
+  }
+  const { valuesForWhichTableDataWasFetched } = store
+
+  // only fetch if not yet fetched
+  if (
+    valuesForWhichTableDataWasFetched.tpopForAp &&
+    valuesForWhichTableDataWasFetched.tpopForAp.ApArtId &&
+    valuesForWhichTableDataWasFetched.tpopForAp.ApArtId.includes(apArtId)
+  ) {
+    return
   }
 
   const url = `${apiBaseUrl}/tpopForAp/${apArtId}`
@@ -23,12 +44,12 @@ export default (store, apArtId) => {
     .toArray()
     .then((data) => {
       if (data) {
-        writeToStore(store, data)
+        writeToStore(store, data, apArtId)
       }
     })
     .then(() => axios.get(url))
     .then(({ data }) => {
-      writeToStore(store, data)
+      writeToStore(store, data, apArtId)
       app.db.tpop.bulkPut(data)
     })
     .catch(error => new Error(`error fetching tpop for ap ${apArtId}:`, error))
