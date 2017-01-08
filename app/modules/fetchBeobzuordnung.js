4e92ee7079d93bdbbf3f2a87778dd8a64ba2aa0f
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-08 15:09:26 +0100

    enhance loading only once

--- app/modules/fetchBeobzuordnung.js
+++ app/modules/fetchBeobzuordnung.js
@@ -4,7 +4,8 @@ import app from 'ampersand-app'
 
 import apiBaseUrl from './apiBaseUrl'
 
-const writeToStore = (store, data) => {
+const writeToStore = (store, data, apArtId) => {
+  const { valuesForWhichTableDataWasFetched } = store
   transaction(() => {
     data.forEach((zuordnung) => {
       // set computed value "beob_bereitgestellt"
@@ -26,13 +27,34 @@ const writeToStore = (store, data) => {
     })
   })
   store.table.beobzuordnungLoading = false
+  // record that data was fetched for this value
+  if (!valuesForWhichTableDataWasFetched.beobzuordnung) {
+    valuesForWhichTableDataWasFetched.beobzuordnung = {}
+  }
+  if (!valuesForWhichTableDataWasFetched.beobzuordnung.NO_ISFS) {
+    valuesForWhichTableDataWasFetched.beobzuordnung.NO_ISFS = []
+  }
+  if (!valuesForWhichTableDataWasFetched.beobzuordnung.NO_ISFS.includes(apArtId)) {
+    valuesForWhichTableDataWasFetched.beobzuordnung.NO_ISFS.push(apArtId)
+  }
 }
 
 export default (store, apArtId) => {
   // console.log(`module fetchBeobzuordnung: apArtId:`, apArtId)
+  const { valuesForWhichTableDataWasFetched } = store
   if (!apArtId) {
     return new Error(`action fetchBeobzuordnung: apArtId must be passed`)
   }
+
+  // only fetch if not yet fetched
+  if (
+    valuesForWhichTableDataWasFetched.beobzuordnung &&
+    valuesForWhichTableDataWasFetched.beobzuordnung.NO_ISFS &&
+    valuesForWhichTableDataWasFetched.beobzuordnung.NO_ISFS.includes(apArtId)
+  ) {
+    return
+  }
+
   const url = `${apiBaseUrl}/beobzuordnung/${apArtId}`
   store.table.beobzuordnungLoading = true
   app.db.beobzuordnung
@@ -40,12 +62,12 @@ export default (store, apArtId) => {
     .then((data) => {
       if (data.length > 0) {
         // console.log(`fetching beobzuordnung from idb`)
-        writeToStore(store, data)
+        writeToStore(store, data, apArtId)
       }
     })
     .then(() => axios.get(url))
     .then(({ data }) => {
-      writeToStore(store, data)
+      writeToStore(store, data, apArtId)
       app.db.beobzuordnung.bulkPut(data)
     })
     .catch(error => new Error(`error fetching table beobzuordnung:`, error))
