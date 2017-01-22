1622ad0043bc6e7d26d65bad706dc953e9d2a5fd
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-22 22:05:34 +0100

    recompose

--- app/components/Projekte/index.js
+++ app/components/Projekte/index.js
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react'
 import { observer, inject } from 'mobx-react'
 import clone from 'lodash/clone'
-import remove from 'lodash/remove'
 import styled from 'styled-components'
+import compose from 'recompose/compose'
 
 import StrukturbaumContainer from './StrukturbaumContainer'
 import DeleteDatasetModal from './DeleteDatasetModal'
@@ -29,64 +29,46 @@ const KarteContainer = styled.div`
   height: 100%;
 `
 
-@inject(`store`)
-@observer
-class Projekte extends React.Component { // eslint-disable-line react/prefer-stateless-function
+const enhance = compose(
+  inject(`store`),
+  observer
+)
 
-  static propTypes = {
-    store: PropTypes.object,
-  }
+const Projekte = ({ store }) => {
+  const projekteTabs = clone(store.urlQuery.projekteTabs)
+  const strukturbaumIsVisible = projekteTabs && projekteTabs.includes(`strukturbaum`)
+  const datenIsVisible = projekteTabs && projekteTabs.includes(`daten`)
+  const karteIsVisible = projekteTabs && projekteTabs.includes(`karte`)
+  const deleteDatasetModalIsVisible = !!store.datasetToDelete.id
 
-  constructor() {
-    super()
-    this.onClickButton = this.onClickButton.bind(this)
-  }
-
-  onClickButton(name) {
-    const { store } = this.props
-    const projekteTabs = store.urlQuery.projekteTabs ? clone(store.urlQuery.projekteTabs) : []
-    const isVisible = projekteTabs && projekteTabs.includes(name)
-    if (isVisible) {
-      remove(projekteTabs, el => el === name)
-    } else {
-      projekteTabs.push(name)
-    }
-    store.setUrlQuery(`projekteTabs`, projekteTabs)
-  }
-
-  render() {
-    const { store } = this.props
-    const projekteTabs = clone(store.urlQuery.projekteTabs)
-    const strukturbaumIsVisible = projekteTabs && projekteTabs.includes(`strukturbaum`)
-    const datenIsVisible = projekteTabs && projekteTabs.includes(`daten`)
-    const karteIsVisible = projekteTabs && projekteTabs.includes(`karte`)
-    const deleteDatasetModalIsVisible = !!store.datasetToDelete.id
-
-    return (
-      <Container>
-        <Content>
-          {
-            strukturbaumIsVisible
-              && <StrukturbaumContainer />
-          }
-          {
-            datenIsVisible
-              && <Daten />
-          }
-          {
-            karteIsVisible &&
-            <KarteContainer>
-              <Karte />
-            </KarteContainer>
-          }
-          {
-            deleteDatasetModalIsVisible &&
-            <DeleteDatasetModal />
-          }
-        </Content>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <Content>
+        {
+          strukturbaumIsVisible
+            && <StrukturbaumContainer />
+        }
+        {
+          datenIsVisible
+            && <Daten />
+        }
+        {
+          karteIsVisible &&
+          <KarteContainer>
+            <Karte />
+          </KarteContainer>
+        }
+        {
+          deleteDatasetModalIsVisible &&
+          <DeleteDatasetModal />
+        }
+      </Content>
+    </Container>
+  )
 }
 
-export default Projekte
+Projekte.PropTypes = {
+  store: PropTypes.object.isRequired,
+}
+
+export default enhance(Projekte)
