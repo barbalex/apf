c0a52663cd0ad52de0a82f1d6387e900b667bd2d
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-23 23:30:59 +0100

    recompose

--- app/components/shared/RadioButtonGroupWithInfo.js
+++ app/components/shared/RadioButtonGroupWithInfo.js
@@ -1,54 +1,50 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import styled from 'styled-components'
 
 import RadioButtonGroup from './RadioButtonGroup'
 import InfoWithPopover from './InfoWithPopover'
 
-@observer
-class radioButtonGroupWithInfo extends Component { // eslint-disable-line react/prefer-stateless-function
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const ButtonGroup = styled.div`
+  flex-grow: 1;
+`
 
-  static propTypes = {
-    fieldName: PropTypes.string.isRequired,
-    value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
-    updatePropertyInDb: PropTypes.func.isRequired,
-    popover: PropTypes.element,
-  }
+const RadioButtonGroupWithInfo = ({
+  fieldName,
+  value,
+  dataSource,
+  updatePropertyInDb,
+  popover,
+}) =>
+  <Container>
+    <ButtonGroup>
+      <RadioButtonGroup
+        fieldName={fieldName}
+        value={value}
+        dataSource={dataSource}
+        updatePropertyInDb={updatePropertyInDb}
+      />
+    </ButtonGroup>
+    <InfoWithPopover>
+      {popover}
+    </InfoWithPopover>
+  </Container>
 
-  render() {
-    const {
-      fieldName,
-      value,
-      dataSource,
-      updatePropertyInDb,
-      popover,
-    } = this.props
-    const Container = styled.div`
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    `
-    const ButtonGroup = styled.div`
-      flex-grow: 1;
-    `
-
-    return (
-      <Container>
-        <ButtonGroup>
-          <RadioButtonGroup
-            fieldName={fieldName}
-            value={value}
-            dataSource={dataSource}
-            updatePropertyInDb={updatePropertyInDb}
-          />
-        </ButtonGroup>
-        <InfoWithPopover>
-          {popover}
-        </InfoWithPopover>
-      </Container>
-    )
-  }
+RadioButtonGroupWithInfo.propTypes = {
+  fieldName: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
+  updatePropertyInDb: PropTypes.func.isRequired,
+  popover: PropTypes.element.isRequired,
 }
 
-export default radioButtonGroupWithInfo
+RadioButtonGroupWithInfo.defaultProps = {
+  value: ``,
+}
+
+export default observer(RadioButtonGroupWithInfo)
