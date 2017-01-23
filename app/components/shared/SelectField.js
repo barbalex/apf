78865c3432b619cf6c8868a8f3e5e2a126dcda61
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-23 23:11:44 +0100

    recompose

--- app/components/shared/SelectField.js
+++ app/components/shared/SelectField.js
@@ -1,48 +1,57 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
 
-@observer
-class MySelectField extends Component { // eslint-disable-line react/prefer-stateless-function
+const enhance = compose(
+  withHandlers({
+    onChange: props => (event, key, payload) =>
+      props.updatePropertyInDb(props.fieldName, payload),
+  }),
+  observer
+)
 
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    fieldName: PropTypes.string.isRequired,
-    value: PropTypes.any,
-    dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
-    valueProp: PropTypes.string.isRequired,
-    labelProp: PropTypes.string.isRequired,
-    updatePropertyInDb: PropTypes.func.isRequired,
-  }
+const MySelectField = ({
+  label,
+  value,
+  dataSource,
+  valueProp,
+  labelProp,
+  onChange,
+}) =>
+  <SelectField
+    floatingLabelText={label}
+    value={value || ``}
+    fullWidth
+    onChange={onChange}
+  >
+    {
+      dataSource.map(e =>
+        <MenuItem
+          value={e[valueProp]}
+          primaryText={e[labelProp]}
+          key={e[valueProp]}
+        />
+      )
+    }
+  </SelectField>
 
-  render() {
-    const {
-      label,
-      fieldName,
-      value,
-      dataSource,
-      valueProp,
-      labelProp,
-      updatePropertyInDb,
-    } = this.props
-    return (
-      <SelectField
-        floatingLabelText={label}
-        value={value || ``}
-        fullWidth
-        onChange={(event, key, payload) =>
-          updatePropertyInDb(fieldName, payload)
-        }
-      >
-        {
-          dataSource.map((e, index) =>
-            <MenuItem value={e[valueProp]} primaryText={e[labelProp]} key={index} />
-          )
-        }
-      </SelectField>
-    )
-  }
+
+MySelectField.propTypes = {
+  label: PropTypes.string.isRequired,
+  fieldName: PropTypes.string.isRequired,
+  value: PropTypes.any,
+  dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
+  valueProp: PropTypes.string.isRequired,
+  labelProp: PropTypes.string.isRequired,
+  updatePropertyInDb: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
-export default MySelectField
+MySelectField.defaultProps = {
+  value: ``,
+}
+
+export default enhance(MySelectField)
