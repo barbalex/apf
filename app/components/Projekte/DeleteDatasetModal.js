294a537e09923e23cd83bd547105154384e16bde
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-24 12:05:29 +0100

    recompose

--- app/components/Projekte/DeleteDatasetModal.js
+++ app/components/Projekte/DeleteDatasetModal.js
@@ -2,62 +2,61 @@ import React, { PropTypes } from 'react'
 import { inject } from 'mobx-react'
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
 
 import tables from '../../modules/tables'
 
-@inject(`store`)
-class DatasetDeleteModal extends React.Component { // eslint-disable-line react/prefer-stateless-function
+const enhance = compose(
+  inject(`store`),
+  withHandlers({
+    onClickAbort: props => () =>
+      props.deleteDatasetAbort(),
+    onClickDelete: props => () =>
+      props.deleteDatasetExecute(),
+  })
+)
 
-  static propTypes = {
-    store: PropTypes.object,
+const DatasetDeleteModal = ({
+  store,
+  onClickAbort,
+  onClickDelete,
+}) => {
+  const actions = [
+    <FlatButton
+      label="Abbrechen"
+      onTouchTap={onClickAbort}
+    />,
+    <FlatButton
+      label="Löschen"
+      primary
+      keyboardFocused
+      onTouchTap={onClickDelete}
+    />,
+  ]
+  const table = tables.find(t =>
+    t.table === store.datasetToDelete.table
+  )
+  let tableName = null
+  if (table && table.labelSingular) {
+    tableName = table.labelSingular
   }
 
-  constructor() {
-    super()
-    this.onClickAbort = this.onClickAbort.bind(this)
-    this.onClickDelete = this.onClickDelete.bind(this)
-  }
-
-  onClickAbort() {
-    const { store } = this.props
-    store.deleteDatasetAbort()
-  }
-
-  onClickDelete() {
-    const { store } = this.props
-    store.deleteDatasetExecute()
-  }
-
-  render() {
-    const { store } = this.props
-    const actions = [
-      <FlatButton
-        label="Abbrechen"
-        onTouchTap={this.onClickAbort}
-      />,
-      <FlatButton
-        label="Löschen"
-        primary
-        keyboardFocused
-        onTouchTap={this.onClickDelete}
-      />,
-    ]
-    const table = tables.find(t => t.table === store.datasetToDelete.table)
-    let tableName = null
-    if (table && table.labelSingular) {
-      tableName = table.labelSingular
-    }
-
-    return (
-      <Dialog
-        actions={actions}
-        modal
-        open={!!store.datasetToDelete.id}
-      >
-        {`${tableName ? `${tableName} "` : ``}${store.datasetToDelete.label}${tableName ? `"` : ``} löschen?`}
-      </Dialog>
-    )
-  }
+  return (
+    <Dialog
+      actions={actions}
+      modal
+      open={!!store.datasetToDelete.id}
+    >
+      {`${tableName ? `${tableName} "` : ``}${store.datasetToDelete.label}${tableName ? `"` : ``} löschen?`}
+    </Dialog>
+  )
 }
 
-export default DatasetDeleteModal
+DatasetDeleteModal.propTypes = {
+  store: PropTypes.object.isRequired,
+  onClickAbort: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired,
+}
+
+export default enhance(DatasetDeleteModal)
