1c1928385037a99195cf0596f6737c97ccab1909
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-24 11:06:44 +0100

    recompose

--- app/components/shared/InfoWithPopover.js
+++ app/components/shared/InfoWithPopover.js
@@ -1,71 +1,75 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import Popover from 'material-ui/Popover'
 import FontIcon from 'material-ui/FontIcon'
 import styled from 'styled-components'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
 
-@observer
-class InfoWithPopover extends Component {
+const enhance = compose(
+  withState(`popupOpen`, `changePopupOpen`, false),
+  withState(`popupAnchorEl`, `changePopupAnchorEl`, null),
+  withHandlers({
+    onClickFontIcon: props => (event) => {
+      event.preventDefault()
+      props.changePopupOpen(!props.popupOpen)
+      props.changePopupAnchorEl(event.currentTarget)
+    },
+    onRequestClosePopover: props => () =>
+      props.changePopupOpen(false)
+    ,
+  }),
+  observer
+)
 
-  static propTypes = {
-    children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
-  }
+const InfoWithPopover = ({
+  popupOpen,
+  popupAnchorEl,
+  onRequestClosePopover,
+  onClickFontIcon,
+}) =>
+  <div>
+    <FontIcon
+      id="iconEl"
+      className="material-icons"
+      onClick={onClickFontIcon}
+      style={{
+        cursor: `pointer`,
+        pointerEvents: `auto`,
+      }}
+    >
+      info_outline
+    </FontIcon>
+    <Popover
+      open={popupOpen}
+      anchorEl={popupAnchorEl}
+      anchorOrigin={{ horizontal: `left`, vertical: `top` }}
+      targetOrigin={{ horizontal: `left`, vertical: `bottom` }}
+      animated
+      autoCloseWhenOffScreen
+      canAutoPosition
+      onRequestClose={onRequestClosePopover}
+      style={{
+        borderRadius: `4px`,
+      }}
+    >
+      {this.props.children}
+    </Popover>
+  </div>
 
-  constructor() {
-    super()
-    this.state = {
-      popupOpen: false,
-      popupAnchorEl: null,
-    }
-  }
-
-  render() {
-    const {
-      popupOpen,
-      popupAnchorEl,
-    } = this.state
-
-    // DONT use styled because popupAnchorEl then seems to be at 0 0!!!!
-
-    return (
-      <div>
-        <FontIcon
-          id="iconEl"
-          className="material-icons"
-          onClick={(event) => {
-            event.preventDefault()
-            this.setState({
-              popupOpen: !popupOpen,
-              popupAnchorEl: event.currentTarget,
-            })
-          }}
-          style={{
-            cursor: `pointer`,
-            pointerEvents: `auto`,
-          }}
-        >
-          info_outline
-        </FontIcon>
-        <Popover
-          open={popupOpen}
-          anchorEl={popupAnchorEl}
-          anchorOrigin={{ horizontal: `left`, vertical: `top` }}
-          targetOrigin={{ horizontal: `left`, vertical: `bottom` }}
-          animated
-          autoCloseWhenOffScreen
-          canAutoPosition
-          onRequestClose={() => {
-            this.setState({ popupOpen: false })
-          }}
-          style={{
-            borderRadius: `4px`,
-          }}
-        >
-          {this.props.children}
-        </Popover>
-      </div>
-    )
-  }
+InfoWithPopover.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+  popupOpen: PropTypes.bool.isRequired,
+  changePopupOpen: PropTypes.func.isRequired,
+  popupAnchorEl: PropTypes.object,
+  changePopupAnchorEl: PropTypes.func.isRequired,
+  onClickFontIcon: PropTypes.func.isRequired,
+  onRequestClosePopover: PropTypes.func.isRequired,
 }
 
-export default InfoWithPopover
+InfoWithPopover.defaultProps = {
+  popupAnchorEl: null,
+}
+
+export default enhance(InfoWithPopover)
