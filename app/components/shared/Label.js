91463b32cd9205eb4b2f54a629e2312f97465e56
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-24 10:59:25 +0100

    recompose

--- app/components/shared/Label.js
+++ app/components/shared/Label.js
@@ -1,32 +1,24 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { observer } from 'mobx-react'
 import styled from 'styled-components'
 
-@observer
-class Label extends Component { // eslint-disable-line react/prefer-stateless-function
+const StyledLabel = styled.div`
+  margin-top: 10px;
+  cursor: text;
+  font-size: 12px;
+  color: rgba(255, 255, 255, 0.498039);
+  pointer-events: none;
+  user-select: none;
+  padding-bottom: 8px;
+`
 
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-  }
+const Label = ({ label }) =>
+  <StyledLabel>
+    {label}
+  </StyledLabel>
 
-  render() {
-    const { label } = this.props
-    const StyledLabel = styled.div`
-      margin-top: 10px;
-      cursor: text;
-      font-size: 12px;
-      color: rgba(255, 255, 255, 0.498039);
-      pointer-events: none;
-      user-select: none;
-      padding-bottom: 8px;
-    `
-
-    return (
-      <StyledLabel>
-        {label}
-      </StyledLabel>
-    )
-  }
+Label.propTypes = {
+  label: PropTypes.string.isRequired,
 }
 
-export default Label
+export default observer(Label)
