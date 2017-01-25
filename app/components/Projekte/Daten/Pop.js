60405fbea9436810d46b1618abd1329c5e58612b
Alexander Gabriel <alex.barbalex@gmail.com>
2017-01-25 16:28:17 +0100

    compose

--- app/components/Projekte/Daten/Pop.js
+++ app/components/Projekte/Daten/Pop.js
@@ -1,6 +1,8 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { observer, inject } from 'mobx-react'
 import styled from 'styled-components'
+import compose from 'recompose/compose'
+import withProps from 'recompose/withProps'
 
 import TextField from '../../shared/TextField'
 import InfoWithPopover from '../../shared/InfoWithPopover'
@@ -42,102 +44,106 @@ const FieldWithInfoContainer = styled.div`
   align-items: flex-end;
 `
 
-@inject(`store`)
-@observer
-class Pop extends Component { // eslint-disable-line react/prefer-stateless-function
-
-  static propTypes = {
-    store: PropTypes.object,
-  }
-
-  get apJahr() {
-    const { store } = this.props
+const enhance = compose(
+  inject(`store`),
+  withProps((props) => {
+    const { store } = props
     const { activeDataset } = store
-    return store.table.ap.get(activeDataset.row.ApArtId).ApJahr
-  }
+    const apJahr = store.table.ap.get(activeDataset.row.ApArtId).ApJahr
+    return { apJahr, activeDataset }
+  }),
+  observer
+)
 
-  render() {
-    const { store } = this.props
-    const { activeDataset } = store
+const Pop = ({
+  store,
+  activeDataset,
+  apJahr,
+}) =>
+  <Container>
+    <FormTitle title="Population" />
+    <FieldsContainer>
+      <TextField
+        label="Nr."
+        fieldName="PopNr"
+        value={activeDataset.row.PopNr}
+        errorText={activeDataset.valid.PopNr}
+        type="number"
+        updateProperty={store.updateProperty}
+        updatePropertyInDb={store.updatePropertyInDb}
+      />
+      <FieldWithInfoContainer>
+        <TextField
+          label="Name"
+          fieldName="PopName"
+          value={activeDataset.row.PopName}
+          errorText={activeDataset.valid.PopName}
+          type="text"
+          updateProperty={store.updateProperty}
+          updatePropertyInDb={store.updatePropertyInDb}
+        />
+        <InfoWithPopover>
+          <PopoverContentRow>
+            Dieses Feld möglichst immer ausfüllen
+          </PopoverContentRow>
+        </InfoWithPopover>
+      </FieldWithInfoContainer>
+      <Status
+        apJahr={apJahr}
+        herkunftFieldName="PopHerkunft"
+        herkunftValue={activeDataset.row.PopHerkunft}
+        bekanntSeitFieldName="PopBekanntSeit"
+        bekanntSeitValue={activeDataset.row.PopBekanntSeit}
+        bekanntSeitValid={activeDataset.valid.PopBekanntSeit}
+        updateProperty={store.updateProperty}
+        updatePropertyInDb={store.updatePropertyInDb}
+      />
+      <Label label="Status unklar" />
+      <RadioButton
+        fieldName="PopHerkunftUnklar"
+        value={activeDataset.row.PopHerkunftUnklar}
+        updatePropertyInDb={store.updatePropertyInDb}
+      />
+      <TextField
+        label="Begründung"
+        fieldName="PopHerkunftUnklarBegruendung"
+        value={activeDataset.row.PopHerkunftUnklarBegruendung}
+        errorText={activeDataset.valid.PopHerkunftUnklarBegruendung}
+        type="text"
+        multiLine
+        fullWidth
+        updateProperty={store.updateProperty}
+        updatePropertyInDb={store.updatePropertyInDb}
+      />
+      <TextField
+        label="X-Koordinaten"
+        fieldName="PopXKoord"
+        value={activeDataset.row.PopXKoord}
+        errorText={activeDataset.valid.PopXKoord}
+        type="number"
+        updateProperty={store.updateProperty}
+        updatePropertyInDb={store.updatePropertyInDb}
+      />
+      <TextField
+        label="Y-Koordinaten"
+        fieldName="PopYKoord"
+        value={activeDataset.row.PopYKoord}
+        errorText={activeDataset.valid.PopYKoord}
+        type="number"
+        updateProperty={store.updateProperty}
+        updatePropertyInDb={store.updatePropertyInDb}
+      />
+    </FieldsContainer>
+  </Container>
 
-    return (
-      <Container>
-        <FormTitle title="Population" />
-        <FieldsContainer>
-          <TextField
-            label="Nr."
-            fieldName="PopNr"
-            value={activeDataset.row.PopNr}
-            errorText={activeDataset.valid.PopNr}
-            type="number"
-            updateProperty={store.updateProperty}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-          <FieldWithInfoContainer>
-            <TextField
-              label="Name"
-              fieldName="PopName"
-              value={activeDataset.row.PopName}
-              errorText={activeDataset.valid.PopName}
-              type="text"
-              updateProperty={store.updateProperty}
-              updatePropertyInDb={store.updatePropertyInDb}
-            />
-            <InfoWithPopover>
-              <PopoverContentRow>
-                Dieses Feld möglichst immer ausfüllen
-              </PopoverContentRow>
-            </InfoWithPopover>
-          </FieldWithInfoContainer>
-          <Status
-            apJahr={this.apJahr}
-            herkunftFieldName="PopHerkunft"
-            herkunftValue={activeDataset.row.PopHerkunft}
-            bekanntSeitFieldName="PopBekanntSeit"
-            bekanntSeitValue={activeDataset.row.PopBekanntSeit}
-            bekanntSeitValid={activeDataset.valid.PopBekanntSeit}
-            updateProperty={store.updateProperty}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-          <Label label="Status unklar" />
-          <RadioButton
-            fieldName="PopHerkunftUnklar"
-            value={activeDataset.row.PopHerkunftUnklar}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-          <TextField
-            label="Begründung"
-            fieldName="PopHerkunftUnklarBegruendung"
-            value={activeDataset.row.PopHerkunftUnklarBegruendung}
-            errorText={activeDataset.valid.PopHerkunftUnklarBegruendung}
-            type="text"
-            multiLine
-            fullWidth
-            updateProperty={store.updateProperty}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-          <TextField
-            label="X-Koordinaten"
-            fieldName="PopXKoord"
-            value={activeDataset.row.PopXKoord}
-            errorText={activeDataset.valid.PopXKoord}
-            type="number"
-            updateProperty={store.updateProperty}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-          <TextField
-            label="Y-Koordinaten"
-            fieldName="PopYKoord"
-            value={activeDataset.row.PopYKoord}
-            errorText={activeDataset.valid.PopYKoord}
-            type="number"
-            updateProperty={store.updateProperty}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-        </FieldsContainer>
-      </Container>
-    )
-  }
+Pop.propTypes = {
+  store: PropTypes.object.isRequired,
+  activeDataset: PropTypes.object.isRequired,
+  apJahr: PropTypes.number,
 }
 
-export default Pop
+Pop.defaultProps = {
+  apJahr: ``,
+}
+
+export default enhance(Pop)
