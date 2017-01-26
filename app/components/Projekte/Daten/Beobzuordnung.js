86a079989dd63986807328f121c3f27b2733b506
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-26 11:06:20 +0100

    recompose

--- app/components/Projekte/Daten/Beobzuordnung.js
+++ app/components/Projekte/Daten/Beobzuordnung.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { observer, inject } from 'mobx-react'
 import styled from 'styled-components'
 import sortBy from 'lodash/sortBy'
@@ -60,150 +60,144 @@ const enhance = compose(
   observer
 )
 
-class Beob extends Component { // eslint-disable-line react/prefer-stateless-function
+const getBeob = ({ store }) => {
+  const { activeDataset } = store
+  const beobzuordnung = activeDataset.row
+  const beob = (
+    beobzuordnung.QuelleId === 1 ?
+    store.table.beob_evab.get(beobzuordnung.NO_NOTE) :
+    store.table.beob_infospezies.get(beobzuordnung.NO_NOTE)
+  )
+  return beob
+}
 
-  static propTypes = {
-    store: PropTypes.object.isRequired,
-    typ: PropTypes.string.isRequired,
+const getTpopZuordnenSource = ({ store }) => {
+  const { activeDataset } = store
+  const beobzuordnung = activeDataset.row
+  // get all popIds of active ap
+  const popList = Array.from(store.table.pop.values())
+    .filter(p => p.ApArtId === store.activeUrlElements.ap)
+  const popIdList = popList.map(p => p.PopId)
+  // get all tpop
+  let tpopList = Array.from(store.table.tpop.values())
+    // of active ap
+    .filter(t => popIdList.includes(t.PopId))
+    // with coordinates
+    .filter(t => t.TPopXKoord && t.TPopYKoord)
+  // calculate their distance to this beobzuordnung
+  const beob = getBeob({ store })
+  // beob loads later
+  // prevent an error occuring if it does not yet exist
+  // by passing back an empty array
+  if (!beob) {
+    return []
   }
+  const beobX = (
+    beobzuordnung.QuelleId === 2 ?
+    beob.FNS_XGIS :
+    beob.COORDONNEE_FED_E
+  )
+  const beobY = (
+    beobzuordnung.QuelleId === 2 ?
+    beob.FNS_YGIS :
+    beob.COORDONNEE_FED_N
+  )
+  tpopList.forEach((t) => {
+    const dX = Math.abs(beobX - t.TPopXKoord)
+    const dY = Math.abs(beobY - t.TPopYKoord)
+    t.distance = Math.round(((dX ** 2) + (dY ** 2)) ** 0.5)
+    t.popNr = store.table.pop.get(t.PopId).PopNr
+    // build label
+    t.label = `${t.distance}m: ${t.popNr}/${t.TPopNr}, ${t.TPopFlurname}`
+  })
+  // order them by distance
+  tpopList = sortBy(tpopList, `distance`)
+  // return array of TPopId, label
+  return tpopList.map(t => ({
+    value: t.TPopId,
+    label: t.label,
+  }))
+}
 
-  get beob() {
-    const { store } = this.props
-    const { activeDataset } = store
-    const beobzuordnung = activeDataset.row
-    const beob = (
-      beobzuordnung.QuelleId === 1 ?
-      store.table.beob_evab.get(beobzuordnung.NO_NOTE) :
-      store.table.beob_infospezies.get(beobzuordnung.NO_NOTE)
-    )
-    return beob
-  }
-
-  get tpopZuordnenSource() {
-    const { store } = this.props
-    const { activeDataset } = store
-    const beobzuordnung = activeDataset.row
-    // get all popIds of active ap
-    const popList = Array.from(store.table.pop.values())
-      .filter(p => p.ApArtId === store.activeUrlElements.ap)
-    const popIdList = popList.map(p => p.PopId)
-    // get all tpop
-    let tpopList = Array.from(store.table.tpop.values())
-      // of active ap
-      .filter(t => popIdList.includes(t.PopId))
-      // with coordinates
-      .filter(t => t.TPopXKoord && t.TPopYKoord)
-    // calculate their distance to this beobzuordnung
-    const beob = this.beob
-    // beob loads later
-    // prevent an error occuring if it does not yet exist
-    // by passing back an empty array
-    if (!beob) {
-      return []
-    }
-    const beobX = (
-      beobzuordnung.QuelleId === 2 ?
-      beob.FNS_XGIS :
-      beob.COORDONNEE_FED_E
-    )
-    const beobY = (
-      beobzuordnung.QuelleId === 2 ?
-      beob.FNS_YGIS :
-      beob.COORDONNEE_FED_N
-    )
-    tpopList.forEach((t) => {
-      const dX = Math.abs(beobX - t.TPopXKoord)
-      const dY = Math.abs(beobY - t.TPopYKoord)
-      t.distance = Math.round(((dX ** 2) + (dY ** 2)) ** 0.5)
-      t.popNr = store.table.pop.get(t.PopId).PopNr
-      // build label
-      t.label = `${t.distance}m: ${t.popNr}/${t.TPopNr}, ${t.TPopFlurname}`
+const getBeobFields = ({ store }) => {
+  const beob = getBeob({ store })
+  const beobFields = []
+  if (beob) {
+    Object.keys(beob).forEach((key) => {
+      const value = beob[key]
+      if (value || value === 0) {
+        beobFields.push(
+          <div key={key}>
+            <TextFieldNonUpdatable
+              label={key}
+              value={value}
+            />
+          </div>
+        )
+      }
     })
-    // order them by distance
-    tpopList = sortBy(tpopList, `distance`)
-    // return array of TPopId, label
-    return tpopList.map(t => ({
-      value: t.TPopId,
-      label: t.label,
-    }))
   }
+  return beobFields
+}
 
-  get beobFields() {
-    const beob = this.beob
-    const beobFields = []
-    if (beob) {
-      Object.keys(beob).forEach((key) => {
-        const value = beob[key]
-        if (value || value === 0) {
-          beobFields.push(
-            <div key={key}>
-              <TextFieldNonUpdatable
-                label={key}
-                value={value}
+const Beob = ({ store }) => {
+  const { activeDataset } = store
+  const beobzuordnung = activeDataset.row
+  const beobTitle = (
+    beobzuordnung.QuelleId === 1 ?
+    `Informationen aus EvAB (nicht veränderbar)` :
+    `Informationen aus Infospezies (nicht veränderbar)`
+  )
+  const showTPopId = activeDataset.row.BeobNichtZuordnen !== 1
+
+  return (
+    <Container>
+      <FormTitle title="Beobachtung" />
+      <FieldsContainer>
+        <Label label="Nicht zuordnen" />
+        <RadioButtonWithInfo
+          fieldName="BeobNichtZuordnen"
+          value={activeDataset.row.BeobNichtZuordnen}
+          updatePropertyInDb={store.updatePropertyInDb}
+          popover={nichtZuordnenPopover}
+        />
+        {
+          showTPopId &&
+          <div>
+            <Label label="Einer Teilpopulation zuordnen" />
+            <MaxHeightDiv>
+              <RadioButtonGroup
+                fieldName="TPopId"
+                value={activeDataset.row.TPopId}
+                dataSource={getTpopZuordnenSource({ store })}
+                updatePropertyInDb={store.updatePropertyInDb}
               />
-            </div>
-          )
+            </MaxHeightDiv>
+          </div>
         }
-      })
-    }
-    return beobFields
-  }
+        <TextField
+          label="Bemerkungen zur Zuordnung"
+          fieldName="BeobBemerkungen"
+          value={activeDataset.row.BeobBemerkungen}
+          errorText={activeDataset.valid.BeobBemerkungen}
+          type="text"
+          multiLine
+          fullWidth
+          updateProperty={store.updateProperty}
+          updatePropertyInDb={store.updatePropertyInDb}
+        />
+      </FieldsContainer>
+      <FormTitle title={beobTitle} />
+      <FieldsContainer>
+        {getBeobFields({ store })}
+      </FieldsContainer>
+    </Container>
+  )
+}
 
-  render() {
-    const { store } = this.props
-    const { activeDataset } = store
-    const beobzuordnung = activeDataset.row
-    const beobTitle = (
-      beobzuordnung.QuelleId === 1 ?
-      `Informationen aus EvAB (nicht veränderbar)` :
-      `Informationen aus Infospezies (nicht veränderbar)`
-    )
-    const showTPopId = activeDataset.row.BeobNichtZuordnen !== 1
-
-    return (
-      <Container>
-        <FormTitle title="Beobachtung" />
-        <FieldsContainer>
-          <Label label="Nicht zuordnen" />
-          <RadioButtonWithInfo
-            fieldName="BeobNichtZuordnen"
-            value={activeDataset.row.BeobNichtZuordnen}
-            updatePropertyInDb={store.updatePropertyInDb}
-            popover={nichtZuordnenPopover}
-          />
-          {
-            showTPopId &&
-            <div>
-              <Label label="Einer Teilpopulation zuordnen" />
-              <MaxHeightDiv>
-                <RadioButtonGroup
-                  fieldName="TPopId"
-                  value={activeDataset.row.TPopId}
-                  dataSource={this.tpopZuordnenSource}
-                  updatePropertyInDb={store.updatePropertyInDb}
-                />
-              </MaxHeightDiv>
-            </div>
-          }
-          <TextField
-            label="Bemerkungen zur Zuordnung"
-            fieldName="BeobBemerkungen"
-            value={activeDataset.row.BeobBemerkungen}
-            errorText={activeDataset.valid.BeobBemerkungen}
-            type="text"
-            multiLine
-            fullWidth
-            updateProperty={store.updateProperty}
-            updatePropertyInDb={store.updatePropertyInDb}
-          />
-        </FieldsContainer>
-        <FormTitle title={beobTitle} />
-        <FieldsContainer>
-          {this.beobFields}
-        </FieldsContainer>
-      </Container>
-    )
-  }
+Beob.propTypes = {
+  store: PropTypes.object.isRequired,
+  typ: PropTypes.string.isRequired,
 }
 
 export default enhance(Beob)
